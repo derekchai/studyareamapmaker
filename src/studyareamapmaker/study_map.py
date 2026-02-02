a6985cd5ae3aa626edbfd8622fbb2a265eef0928
Derek Chai <chderek9@gmail.com>
2026-02-03 10:42:17 +1300

    Replace Query with Fieldd

--- src/studyareamapmaker/study_map.py
+++ src/studyareamapmaker/study_map.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from typing import Annotated
+from pydantic import BaseModel, Field
 
 class StudyMap(BaseModel):
-    inset_width: Annotated[float, Query(ge=0, le=1)] = 0.3
-    inset_height: Annotated[float, Query(ge=0, le=1)] = 0.3
+    inset_width: float = Field(default=0.3, ge=0, le=1)
+    inset_height: float = Field(default=0.3, ge=0, le=1)
