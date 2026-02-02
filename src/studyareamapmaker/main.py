2d31d7b30e765c2cd9b4aeba8d0aaad6de466f30
Derek Chai <chderek9@gmail.com>
2026-02-03 10:54:49 +1300

    Add toggle for inset map background

--- src/studyareamapmaker/main.py
+++ src/studyareamapmaker/main.py
@@ -10,7 +10,7 @@ def generate_study_area_map(shapefile_path: str,
 
     fig, ax = plt.subplots(figsize=(9, 9))
 
-    _plot_inset_map(ax, shape, map.inset_width, map.inset_height)
+    _plot_inset_map(ax, shape, map)
 
     ax.set_title("Shapefile")
 
@@ -20,22 +20,15 @@ def generate_study_area_map(shapefile_path: str,
 
 def _plot_inset_map(ax: plt.Axes, 
                     shape: gpd.GeoDataFrame,
-                    width: float,
-                    height: float):
-    if width < 0 or width > 1:
-        width = "30%"
-    else:
-        width = f"{width * 100}%"
-
-    if height < 0 or height > 1:
-        height = "30%"
-    else:
-        height = f"{height * 100}%"
+                    map: StudyMap):
+    width = f"{map.inset_width * 100}%"
+    height = f"{map.inset_height * 100}%"
         
     inset: plt.Axes = inset_axes(ax, width=width, height=height)
 
-    inset.set_frame_on(False)  # remove border
-    inset.set_axis_off()  # remove axis ticks, etc.
-    inset.patch.set_alpha(0)  # set transparent background
+    if not map.inset_show_background:
+        inset.set_frame_on(False)  # remove border
+        inset.set_axis_off()  # remove axis ticks, etc.
+        inset.patch.set_alpha(0)  # set transparent background
 
     shape.plot(ax=inset, linewidth=1, edgecolor="k", facecolor="w")
--- src/studyareamapmaker/study_map.py
+++ src/studyareamapmaker/study_map.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field, model_validator
 class StudyMap(BaseModel):
     inset_width: float = Field(default=0.3, ge=0, le=1)
     inset_height: float = Field(default=0.3, ge=0, le=1)
+    inset_show_background: bool = Field(default=False)
 
     @model_validator(mode="before")
     @classmethod
