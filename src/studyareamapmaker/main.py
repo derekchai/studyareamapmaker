634db2a17fc82cca71e24c935ad14aa603886d8b
Derek Chai <chderek9@gmail.com>
2026-02-03 10:20:50 +1300

    Use StudyMap class as parameter

--- src/studyareamapmaker/main.py
+++ src/studyareamapmaker/main.py
@@ -2,13 +2,14 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from io import BytesIO
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from .study_map import StudyMap
 
-def generate_study_area_map(shapefile_path: str) -> bytes:
-    shape: gpd.GeoDataFrame = gpd.read_file(shapefile_path)
+def generate_study_area_map(map: StudyMap) -> bytes:
+    shape: gpd.GeoDataFrame = gpd.read_file(map.shapefile_path)
 
     fig, ax = plt.subplots(figsize=(9, 9))
 
-    _plot_inset_map(ax, shape)
+    _plot_inset_map(ax, shape, map.inset_width, map.inset_height)
 
     ax.set_title("Shapefile")
 
@@ -16,8 +17,21 @@ def generate_study_area_map(shapefile_path: str) -> bytes:
     plt.savefig(buffer, format="png")
     return buffer.getvalue()
 
-def _plot_inset_map(ax: plt.Axes, shape: gpd.GeoDataFrame):
-    inset: plt.Axes = inset_axes(ax, width="30%", height="30%")
+def _plot_inset_map(ax: plt.Axes, 
+                    shape: gpd.GeoDataFrame,
+                    width: float,
+                    height: float):
+    if width < 0 or width > 1:
+        width = "30%"
+    else:
+        width = f"{width * 100}%"
+
+    if height < 0 or height > 1:
+        height = "30%"
+    else:
+        height = f"{height * 100}%"
+        
+    inset: plt.Axes = inset_axes(ax, width=width, height=height)
 
     inset.set_frame_on(False)  # remove border
     inset.set_axis_off()  # remove axis ticks, etc.
