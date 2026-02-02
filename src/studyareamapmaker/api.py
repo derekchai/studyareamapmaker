fbc74c0efab10cea6febfe10dbc55961a7fa9446
Derek Chai <chderek9@gmail.com>
2026-02-02 15:07:49 +1300

    Add test code

--- src/studyareamapmaker/api.py
+++ src/studyareamapmaker/api.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, File, UploadFile
 from io import BytesIO
+from typing import Annotated
+from matplotlib.image import imread
 
 app = FastAPI()
 
@@ -21,3 +23,21 @@ async def get_map(start: float = 0,
     png = buffer.getvalue()
     
     return Response(content=png, media_type="image/png")
+
+@app.post("/identity",
+          responses={
+              200: { "content": { "image/png": {} } }
+          })
+async def get_identity(file: UploadFile):
+    file_contents = await file.read()
+    image = imread(BytesIO(file_contents))
+
+    plt.figure()
+    plt.imshow(image)
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+
+    plt.close()
+
+    return Response(content=buffer.getvalue(), media_type="image/png")
