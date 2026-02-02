800213990d9769a00540ac71e312419590b63def
Derek Chai <chderek9@gmail.com>
2026-02-02 14:50:36 +1300

    Add testing code

--- src/studyareamapmaker/main.py
+++ src/studyareamapmaker/main.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+from fastapi import FastAPI, Response
 from io import BytesIO
 
-def get_map():
-    x = np.arange(0, 5, 0.1)
+app = FastAPI()
+
+@app.get("/map",
+         responses={
+             200: { "content": { "image/png": {} } }
+         })
+async def get_map(start: float = 0,
+                  stop: float = 2 * math.pi):
+    x = np.arange(start, stop, 0.1)
     y = np.sin(x)
     plt.plot(x, y)
     
     buffer = BytesIO()
     plt.savefig(buffer, format="png")
-    return buffer.getvalue()
+    png = buffer.getvalue()
+    
+    return Response(content=png, media_type="image/png")
