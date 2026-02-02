2b77396fb7562fe64f9409e4e1da9f43674c3a6b
Derek Chai <chderek9@gmail.com>
2026-02-03 10:40:44 +1300

    Replace parameters with queries

--- src/studyareamapmaker/study_map.py
+++ src/studyareamapmaker/study_map.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
+from typing import Annotated
 
 class StudyMap(BaseModel):
-    inset_width: float = 0.3
-    inset_height: float = 0.3
+    inset_width: Annotated[float, Query(ge=0, le=1)] = 0.3
+    inset_height: Annotated[float, Query(ge=0, le=1)] = 0.3
