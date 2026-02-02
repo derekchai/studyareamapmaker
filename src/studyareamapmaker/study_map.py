b8fadc01e3ae9f3070c819b671c1b76c1e0d789e
Derek Chai <chderek9@gmail.com>
2026-02-03 10:50:14 +1300

    Add model validator function to StudyMap

--- src/studyareamapmaker/study_map.py
+++ src/studyareamapmaker/study_map.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field
+import json
+from pydantic import BaseModel, Field, model_validator
 
 class StudyMap(BaseModel):
     inset_width: float = Field(default=0.3, ge=0, le=1)
     inset_height: float = Field(default=0.3, ge=0, le=1)
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
