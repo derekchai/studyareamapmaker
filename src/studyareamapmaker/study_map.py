d61713ae118a47d2a916e924192f2ea9965931be
Derek Chai <chderek9@gmail.com>
2026-02-03 17:26:55 +1300

    Move class to start of file

--- src/studyareamapmaker/study_map.py
+++ src/studyareamapmaker/study_map.py
@@ -7,6 +7,29 @@ from shapely.geometry import box
 _WGS84 = "EPSG:4326"
 _SPHERICAL_MERCATOR = "EPSG:3857"
 
+class StudyMapRegion(BaseModel):
+    name: str | None = Field(default=None)
+
+    min_lat: float = Field(ge=-90, le=90)
+    min_lon: float = Field(ge=-180, le=180)
+    max_lat: float = Field(ge=-90, le=90)
+    max_lon: float = Field(ge=-180, le=180)
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.min_lat, self.min_lon, self.max_lat, self.max_lon \
+                = min(self.min_lat, self.max_lat), \
+                min(self.min_lon, self.max_lon), \
+                max(self.min_lat, self.max_lat), \
+                max(self.min_lon, self.max_lon)
+
 class StudyMap(BaseModel):
     study_regions: List[StudyMapRegion] = Field(default=[])
     title: str | None = Field(default=None)
@@ -50,26 +73,3 @@ class StudyMap(BaseModel):
         data_frame["centroid"] = centroids
 
         return data_frame
-
-class StudyMapRegion(BaseModel):
-    name: str | None = Field(default=None)
-
-    min_lat: float = Field(ge=-90, le=90)
-    min_lon: float = Field(ge=-180, le=180)
-    max_lat: float = Field(ge=-90, le=90)
-    max_lon: float = Field(ge=-180, le=180)
-
-    @model_validator(mode="before")
-    @classmethod
-    def validate_to_json(cls, value):
-        if isinstance(value, str):
-            return cls(**json.loads(value))
-        return value
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.min_lat, self.min_lon, self.max_lat, self.max_lon \
-                = min(self.min_lat, self.max_lat), \
-                min(self.min_lon, self.max_lon), \
-                max(self.min_lat, self.max_lat), \
-                max(self.min_lon, self.max_lon)
