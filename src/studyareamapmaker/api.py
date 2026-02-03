00bd88687a62806ef0576dc0a34ab051d0fb8d81
Derek Chai <chderek9@gmail.com>
2026-02-03 14:54:33 +1300

    Temp

--- src/studyareamapmaker/api.py
+++ src/studyareamapmaker/api.py
@@ -1,12 +1,12 @@
 import tempfile
 import shutil
-from fastapi import FastAPI, Response, UploadFile, Request
-from typing import List
+from fastapi import FastAPI, Response, UploadFile, Request, Form
+from typing import List, Annotated
 from pathlib import Path
 from .main import generate_study_area_map 
 from .study_map import StudyMap
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
 
@@ -21,7 +21,7 @@ async def get_index(request: Request):
               200: { "content": { "image/png": {} } }
           })
 async def get_map(shapefiles: List[UploadFile],
-                  study_map: StudyMap):
+                  study_map: Annotated[StudyMap, Form()]):
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir_path = Path(temp_dir)
         main_shp_path = None
